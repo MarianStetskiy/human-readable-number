1cd55af782b9f3ecc6cd41ddc01b8e2e78b98b33
Marian <marian50391@gmail.com>
2022-07-16 14:06:20 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 module.exports = function toReadable(number) {
-	const readableNumber = '';
-	if (number > 0 && number < 10) {
-		switch (number) {
-			case 1: readableNumber = 'one';
-			case 2: readableNumber = 'two';
-			case 3: readableNumber = 'three';
-			case 4: readableNumber = 'four';
-			case 5: readableNumber = 'five';
-			case 6: readableNumber = 'six';
-			case 7: readableNumber = 'seven';
-			case 8: readableNumber = 'eight';
-			case 9: readableNumber = 'nine';
-		}
-		return readableNumber;
+	let result = '';
+	const oneToNine = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	const elevenToNineteen = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	const dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	const hundred = 'hundred';
+	const remDivTen = number % 10;
+	const remDivHundred = number % 100;
+	if (number === 0) {
+		return 'zero';
 	} else if (number > 0 && number < 10) {
-		switch (number) {
-			case 10: readableNumber = 'ten';
-			case 11: readableNumber = 'eleven';
-			case 12: readableNumber = 'twelve';
-			case 13: readableNumber = 'thirteen';
-			case 14: readableNumber = 'fourteen';
-			case 15: readableNumber = 'fifteen';
-			case 16: readableNumber = 'sixteen';
-			case 17: readableNumber = 'seventeen';
-			case 18: readableNumber = 'eighteen';
-			case 19: readableNumber = 'nineteen';
+		return result = oneToNine[number - 1];
+	} else if (number > 10 && number < 20) {
+		return result = elevenToNineteen[(number - 10) - 1];
+	} else if (remDivTen === 0 && number < 100) {
+		return result = dozens[(number / 10) - 1];
+	} else if (remDivTen !== 0 && number < 100) {
+		return result = dozens[((number - remDivTen) / 10) - 1] + ' ' + oneToNine[(remDivTen) - 1];
+	} else if (number >= 100 && remDivHundred === 0) {
+		return result = oneToNine[(number / 100) - 1] + ' ' + hundred;
+	} else if (number > 100 && remDivHundred !== 0) {
+		if (remDivHundred > 0 && remDivHundred < 10) {
+			return result = oneToNine[((number - remDivHundred) / 100) - 1] + ' ' + hundred + ' ' + oneToNine[remDivHundred - 1];
+		} else if (remDivHundred > 10 && remDivHundred < 20) {
+			return result = oneToNine[((number - remDivHundred) / 100) - 1] + ' ' + hundred + ' ' + elevenToNineteen[(remDivHundred - 10) - 1];
+		} else if (remDivHundred % 10 === 0) {
+			return result = oneToNine[((number - remDivHundred) / 100) - 1] + ' ' + hundred + ' ' + dozens[(remDivHundred / 10) - 1];
+		} else if (remDivHundred % 10 !== 0) {
+			return result = oneToNine[((number - remDivHundred) / 100) - 1] + ' ' + hundred + ' ' + dozens[((remDivHundred - (remDivHundred % 10)) / 10) - 1] + ' ' + oneToNine[(remDivHundred % 10) - 1];
 		}
-		return readableNumber;
 	}
 }
